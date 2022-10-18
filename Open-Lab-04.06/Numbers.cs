a37c7f63b8c6328269273ac9d661af72eb960675
Marko <jantulamarko@outlook.com>
2022-10-18 08:40:30 +0200

    Update Numbers.cs

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_04._06
 {
@@ -6,7 +6,27 @@ namespace Open_Lab_04._06
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            int num = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                    num++;
+            }
+
+            int[] uwu = new int[num];
+            int sus = 0;
+
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                if (numbers[j] % 2 == 0)
+                {
+                    uwu[sus] = numbers[j];
+                    sus++;
+                }
+
+
+            }
+            return uwu;
         }
-    }
+}
 }
